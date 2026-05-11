299e499d1544089529e49ae0c6fce422abb4c58d
vitor hagee <vitorhagee@gmail.com>
2026-05-11 12:41:14 -0300

    funcoes

--- funcoes.ts
+++ funcoes.ts
@@ -27,3 +27,62 @@ function saudacao(a:number):string {
 };
 
 console.log(saudacao(22));
+
+console.log("-----------------------------")
+console.log("exercicio 38:");
+console.log("-----------------------------")
+//38.função parouimpar
+
+function parimpar(a:number):string{
+    if(a % 2 === 0){
+        return "Par"
+    }else {
+        return "Impar"
+    };
+};
+
+console.log(parimpar(23));
+
+console.log("-----------------------------")
+console.log("exercicio 39:");
+console.log("-----------------------------")
+//39.função somar array
+
+
+function somaarray(a:number[]):number{
+    let some = a.reduce((a,b) => {return a+b},0 );
+    return some;
+}
+
+let arraynumeros:number[] = [1,2,3,4,5,6,7,8,9,10];
+console.log(somaarray(arraynumeros));
+
+
+console.log("-----------------------------")
+console.log("exercicio 40:");
+console.log("-----------------------------")
+//40. função filtrar usuarios
+
+type usuario = {
+    nome:string;
+    idade:number;
+    cargo:string;
+    cpf?:string;
+};
+
+let usuarios:usuario[] = [
+    {nome:'Vitor', idade:27, cargo:'analista'} ,
+    {nome:'antunes', idade:26, cargo:'faturista'} ,
+    {nome:'magrini', idade:18, cargo:'analista'} ,
+    {nome:'adriano', idade:8, cargo:'carteiro'} ,
+    {nome:'carla', idade:16, cargo:'jogador'} ,
+    {nome:'samara', idade:65, cargo:'analista'} ,
+    {nome:'murilo', idade:13, cargo:'designer'} ,
+];
+
+function filtrausuario(u:usuario[],cargo:string):usuario[]{
+
+   return u.filter((usuario) => usuario.cargo === cargo);
+}   
+
+console.log(filtrausuario(usuarios,"faturista"));
